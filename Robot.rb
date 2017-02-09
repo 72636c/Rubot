13822fc23712eb13bcc394710127c9a30b88419b
Ryan Ling <ryan@outlook.com.au>
2017-02-09 15:06:30 +1100

    Add place command

--- Robot.rb
+++ Robot.rb
@@ -1,9 +1,33 @@
+# external
+
+# represents a toy robot
+
 class Robot
+
+  def place(x, y, orientation)
+    PlacedRobot.new(x, y, orientation)
+  end
+
+  def report
+    "not placed"
+  end
+
+end
+
+# internal
+
+# represents a toy robot that has been placed on a tabletop
+
+class PlacedRobot
 
   def initialize(x, y, orientation)
     @x = x
     @y = y
     @orientation = orientation
+  end
+  
+  def place(x, y, orientation)
+    PlacedRobot.new(x, y, orientation)
   end
 
   def report
@@ -13,15 +37,17 @@ class Robot
 end
 
 if __FILE__ == $0
-  robot = Robot.new(0, 0, :north)
+  robot = Robot.new
   looping = true
   while looping
     input = gets.chomp
-    case input.downcase
-    when "report"
-      puts robot.report
-    when "quit"
+    if input.downcase == "quit"
       looping = false
+    elsif input.downcase.start_with? "place " and input.count(",") == 2
+      parameters = input.downcase.partition("place ").last.split(",")
+      robot = robot.place(parameters[0].strip, parameters[1].strip, parameters[2].strip)
+    elsif input.downcase == "report"
+      puts robot.report
     end
   end
 end
--- Robot_spec.rb
+++ Robot_spec.rb
@@ -3,12 +3,36 @@ require "./Robot"
 
 describe "Robot" do
 
-  before(:all) do
-    @robot = Robot.new(0, 0, :north)
+  describe "not placed" do
+
+    it "responds to report" do
+      @robot = Robot.new
+      expect(@robot.report).to eq("not placed")
+    end
+
+    it "responds to place" do
+      @robot = Robot.new
+      @robot = @robot.place(0, 0, :north)
+      expect(@robot.report).to eq("at (0, 0) facing north")
+    end
+
   end
 
-  it "reports" do
-    expect(@robot.report).to eq("at (0, 0) facing north")
+  describe "placed" do
+
+    it "responds to report" do
+      @robot = Robot.new
+      @robot = @robot.place(0, 0, :north)
+      expect(@robot.report).to eq("at (0, 0) facing north")
+    end
+
+    it "responds to place" do
+      @robot = Robot.new
+      @robot = @robot.place(0, 0, :north)
+      @robot = @robot.place(4, 4, :south)
+      expect(@robot.report).to eq("at (4, 4) facing south")
+    end
+
   end
 
 end
