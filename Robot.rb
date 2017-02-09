9778058153c403d647ccc14a8fc926c0c161ecfd
Ryan Ling <ryan@outlook.com.au>
2017-02-09 16:11:39 +1100

    Add move, left, right commands

--- Robot.rb
+++ Robot.rb
@@ -4,8 +4,37 @@
 
 class Robot
 
+  def self.place(x, y, orientation)
+    case orientation
+    when "north"
+      position = North.new(x, y)
+    when "east"
+      position = East.new(x, y)
+    when "south"
+      position = South.new(x, y)
+    when "west"
+      position = West.new(x, y)
+    else
+      raise "invalid orientation"
+    end
+    
+    PlacedRobot.new(position)
+  end
+  
   def place(x, y, orientation)
-    PlacedRobot.new(x, y, orientation)
+    Robot.place(x, y, orientation)
+  end
+
+  def move
+    self
+  end
+
+  def left
+    self
+  end
+  
+  def right
+    self
   end
 
   def report
@@ -20,18 +49,128 @@ end
 
 class PlacedRobot
 
-  def initialize(x, y, orientation)
-    @x = x
-    @y = y
-    @orientation = orientation
+  def initialize(position)
+    @position = position
   end
   
   def place(x, y, orientation)
-    PlacedRobot.new(x, y, orientation)
+    Robot.place(x, y, orientation)
+  end
+
+  def move
+    PlacedRobot.new(@position.move)
+  end
+
+  def left
+    PlacedRobot.new(@position.left)
+  end
+  
+  def right
+    PlacedRobot.new(@position.right)
+  end
+  
+  def report
+    @position.report
+  end
+
+end
+
+class North
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def move
+    North.new(@x, @y + 1)
+  end
+
+  def left
+    West.new(@x, @y)
+  end
+  
+  def right
+    East.new(@x, @y)
   end
 
   def report
-    "at (#{@x}, #{@y}) facing #{@orientation}"
+    "at (#{@x}, #{@y}) facing north"
+  end
+
+end
+
+class East
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def move
+    East.new(@x + 1, @y)
+  end
+
+  def left
+    North.new(@x, @y)
+  end
+  
+  def right
+    South.new(@x, @y)
+  end
+
+  def report
+    "at (#{@x}, #{@y}) facing east"
+  end
+
+end
+
+class South
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def move
+    South.new(@x, @y - 1)
+  end
+
+  def left
+    East.new(@x, @y)
+  end
+  
+  def right
+    West.new(@x, @y)
+  end
+
+  def report
+    "at (#{@x}, #{@y}) facing south"
+  end
+
+end
+
+class West
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+  
+  def move
+    West.new(@x - 1, @y)
+  end
+
+  def left
+    South.new(@x, @y)
+  end
+  
+  def right
+    North.new(@x, @y)
+  end
+
+  def report
+    "at (#{@x}, #{@y}) facing west"
   end
 
 end
@@ -45,9 +184,17 @@ if __FILE__ == $0
       looping = false
     elsif input.downcase.start_with? "place " and input.count(",") == 2
       parameters = input.downcase.partition("place ").last.split(",")
-      robot = robot.place(parameters[0].strip, parameters[1].strip, parameters[2].strip)
+      robot = robot.place(parameters[0].strip.to_i, parameters[1].strip.to_i, parameters[2].strip)
+    elsif input.downcase == "move"
+      robot = robot.move
+    elsif input.downcase == "left"
+      robot = robot.left
+    elsif input.downcase == "right"
+      robot = robot.right
     elsif input.downcase == "report"
       puts robot.report
+    else
+      puts "invalid command"
     end
   end
 end
--- Robot_spec.rb
+++ Robot_spec.rb
@@ -5,32 +5,71 @@ describe "Robot" do
 
   describe "not placed" do
 
-    it "responds to report" do
+    it "responds to place" do
       @robot = Robot.new
+      @robot = @robot.place(0, 0, "north")
+      expect(@robot.report).to eq("at (0, 0) facing north")
+    end
+
+    it "ignores move" do
+      @robot = Robot.new
+      @robot = @robot.move
       expect(@robot.report).to eq("not placed")
     end
 
-    it "responds to place" do
+    it "ignores left" do
       @robot = Robot.new
-      @robot = @robot.place(0, 0, :north)
-      expect(@robot.report).to eq("at (0, 0) facing north")
+      @robot = @robot.left
+      expect(@robot.report).to eq("not placed")
+    end
+
+    it "ignores right" do
+      @robot = Robot.new
+      @robot = @robot.right
+      expect(@robot.report).to eq("not placed")
+    end
+
+    it "responds to report" do
+      @robot = Robot.new
+      expect(@robot.report).to eq("not placed")
     end
 
   end
 
   describe "placed" do
 
-    it "responds to report" do
-      @robot = Robot.new
-      @robot = @robot.place(0, 0, :north)
-      expect(@robot.report).to eq("at (0, 0) facing north")
-    end
-
     it "responds to place" do
       @robot = Robot.new
-      @robot = @robot.place(0, 0, :north)
-      @robot = @robot.place(4, 4, :south)
+      @robot = @robot.place(0, 0, "north")
+      @robot = @robot.place(4, 4, "south")
       expect(@robot.report).to eq("at (4, 4) facing south")
+    end
+
+    it "responds to move" do
+      @robot = Robot.new
+      @robot = @robot.place(0, 0, "north")
+      @robot = @robot.move
+      expect(@robot.report).to eq("at (0, 1) facing north")
+    end
+
+    it "responds to left" do
+      @robot = Robot.new
+      @robot = @robot.place(0, 0, "north")
+      @robot = @robot.left
+      expect(@robot.report).to eq("at (0, 0) facing west")
+    end
+
+    it "responds to right" do
+      @robot = Robot.new
+      @robot = @robot.place(0, 0, "north")
+      @robot = @robot.right
+      expect(@robot.report).to eq("at (0, 0) facing east")
+    end
+
+    it "responds to report" do
+      @robot = Robot.new
+      @robot = @robot.place(0, 0, "north")
+      expect(@robot.report).to eq("at (0, 0) facing north")
     end
 
   end
